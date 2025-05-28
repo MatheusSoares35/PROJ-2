d20ca13af0101b6fbce2d64b97fb0eac862537fd
MatheusSoares35 <136497637+MatheusSoares35@users.noreply.github.com>
2025-05-28 10:32:52 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,20 @@
+const item = {
+    nome: nome = "peperoni",
+    preco:preco = "11.50"
+};
+const item2 = {
+    nome: nome = "peperoni",
+    preco:preco = "11.50"
+};
+const item3 = {
+    nome: nome = "peperoni",
+    preco:preco = "11.50"
+};
+
+const user = {
+      usuario: usuario = "matt",
+      tipo: tipo = "entregador",
+      pedidos: pedidos = [item, item2, item3]
+    };
+    user.status = "disponível";
+    console.log(user);
