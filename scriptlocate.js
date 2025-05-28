44487896675ba2b3000a764c3aa04b7df832ef7a
MatheusSoares35 <136497637+MatheusSoares35@users.noreply.github.com>
2025-05-27 22:28:27 -0300

    Add files via upload

---
+++ scriptlocate.js
@@ -0,0 +1,11 @@
+function voltarinit(){
+    window.location = "index.html";
+}
+function voltarLogin1(){
+    window.location = "cliente_login.html";
+
+}
+function voltarlogin2(){
+    window.location = "index.html";
+
+}
